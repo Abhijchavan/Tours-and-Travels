01c8056fbef86e68bdd5b3b98dc36d7083da11da
Jubeda2002 <jshaikh00365@gmail.com>
2025-03-21 14:24:11 +0530

    connection.js
    add all inforamtion about database

--- connection.js
+++ connection.js
@@ -1,26 +1,35 @@
-var mysql = require("mysql");
-var util = require("util");
+// var mysql = require("mysql");
+// var util = require("util");
 
-var conn;
+// var conn;
 
-try {
-    conn = mysql.createConnection({
-        host: "bjggynpor9l8tkxsngoc-mysql.services.clever-cloud.com",
-        user: "usd6jvwiumne7mgr",
-        password: "usd6jvwiumne7mgr",
+// try {
+//     conn = mysql.createConnection({
+//         host: "bjggynpor9l8tkxsngoc-mysql.services.clever-cloud.com",
+//         user: "usd6jvwiumne7mgr",
+//         password: "usd6jvwiumne7mgr",
+//         database: "bjggynpor9l8tkxsngoc"
+//     });
+
+//     conn.connect((err) => {
+//         if (err) {
+//             console.error("Database connection failed: ", err);
+//             return;
+//         }
+//         console.log("Connected to database successfully");
+//     });
+// } catch (error) {
+//     console.error("Error while establishing database connection: ", error);
+// }
+// var exe=util.promisify(conn.query).bind(conn);
+// module.exports=exe;
+var mysql=require("mysql");
+var util=require("util");
+var conn=mysql.createConnection({
+    host: "bjggynpor9l8tkxsngoc-mysql.services.clever-cloud.com",
+          user: "usd6jvwiumne7mgr",
+      password: "usd6jvwiumne7mgr",
         database: "bjggynpor9l8tkxsngoc"
-    });
-
-    conn.connect((err) => {
-        if (err) {
-            console.error("Database connection failed: ", err);
-            return;
-        }
-        console.log("Connected to database successfully");
-    });
-} catch (error) {
-    console.error("Error while establishing database connection: ", error);
-}
-
-var exe = util.promisify(conn.query).bind(conn);
-module.exports = exe;
+});
+var exe=util.promisify(conn.query).bind(conn);
+module.exports=exe;
--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,31 @@
 
-var express = require("express")
-var cors = require("cors")
-var session = require('express-session')
-var path = require("path")
-var fs  =require("fs")
-var exe = require("../connection")
-const router = express.Router();
+// var express = require("express")
+// var cors = require("cors")
+// var session = require('express-session')
+// var path = require("path")
+// var fs  =require("fs")
+// var exe = require("../connection")
+// const router = express.Router();
+var express = require("express");
+var exe = require("../connection");
+var router = express.Router();
 
-router.use(cors());
-router.use(express.urlencoded({ extended: true }));
 
-router.use(express.urlencoded({ extended: true }));
+// router.use(cors());
+// router.use(express.urlencoded({ extended: true }));
 
-router.get("/admin", (req, res) => {
-    res.render("/admin/index.html")
+router.get("/",function(req,res){
+    res.render("admin/login.ejs");
+});
+
+
+router.get("/dashboard",function(req,res){
+    res.render("admin/dashboard.ejs");
 });
 
 
 
 
 module.exports = router;
+
+
